c9a07f5572ef5ee70670e28af0fc5e2b08361ed9
erikmint <erik.t3600@hotmail.com>
2023-05-09 23:14:35 -0300

    Create Bubblesort function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let random_arr = [100, 76, 10, 35, 8, 7, 92, 42, 68, 27];
+
+    println!("Bubble Sort: {:?}", bubble_sort(random_arr));
+}
+
+fn bubble_sort(mut unsorted: [u8; 10]) -> [u8; 10]{
+    let mut sorted = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for i in (0..10).rev(){
+        let mut biggest = 0;
+        let mut biggest_index = 0;
+
+        for j in 0..10{
+            if unsorted[j] > biggest {
+                biggest = unsorted[j];
+                biggest_index = j;
+            }
+        }
+        
+        sorted[i] = biggest;
+        unsorted[biggest_index] = u8::MIN;
+    }
+
+    sorted
 }
