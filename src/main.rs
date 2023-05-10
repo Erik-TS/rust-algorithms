f5b4c8b4ae605f6f09c4d8114b41ade8db81cfa7
erikmint <erik.t3600@hotmail.com>
2023-05-09 23:48:58 -0300

    Create Selection Sort function.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,28 @@ fn main() {
     let random_arr = [100, 76, 10, 35, 8, 7, 92, 42, 68, 27];
 
     println!("Bubble Sort: {:?}", bubble_sort(random_arr));
+    println!("Selection Sort: {:?}", selection_sort(random_arr));
+}
+
+fn selection_sort(mut unsorted: [u8; 10]) -> [u8; 10]{
+    let mut sorted = [u8::MAX, u8::MAX, u8::MAX, u8::MAX, u8::MAX, u8::MAX, u8::MAX, u8::MAX, u8::MAX, u8::MAX];
+
+    for i in 0..10 {
+        let mut lowest = u8::MAX;
+        let mut lowest_index = sorted.len() - 1;
+
+        for j in 0..10 {
+            if unsorted[j] < lowest {
+                lowest = unsorted[j];
+                lowest_index = j;
+            }
+        }
+
+        sorted[i] = lowest;
+        unsorted[lowest_index] = u8::MAX;
+    }
+
+    sorted
 }
 
 fn bubble_sort(mut unsorted: [u8; 10]) -> [u8; 10]{
